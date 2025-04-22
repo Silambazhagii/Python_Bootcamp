e848520679b75b35168a16f8660260b2dada9289
Kopperun Silambazhagii <silambazhagii@Silambazhagiis-MacBook-Pro-M1.local>
2025-04-22 16:10:48 +0530

    Day 4: Added models, migrations, and admin setup

---
+++ Day3+/myapp/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+
+class Course(models.Model):
+    title = models.CharField(max_length=100)  
+    description = models.TextField()          
+    duration = models.IntegerField()          
+
+    def __str__(self):
+        return self.title
+
+class Student(models.Model):
+    name = models.CharField(max_length=100)           
+    email = models.EmailField()                       
+    enrolled_course = models.ForeignKey(Course, on_delete=models.CASCADE)  
+
+    def __str__(self):
+        return self.name
