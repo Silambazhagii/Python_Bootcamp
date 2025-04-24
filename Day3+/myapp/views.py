a5a751fab35b6a0206871dbae2818a64e9ba7e0c
Kopperun Silambazhagii <silambazhagii@Silambazhagiis-MacBook-Pro-M1.local>
2025-04-24 18:05:57 +0530

    Day5

--- Day3+/myapp/views.py
+++ Day3+/myapp/views.py
@@ -1,4 +1,15 @@
 from django.shortcuts import render
+from .forms import NameForm
 
 def home(request):
     return render(request, 'home.html')
+
+def get_name(request):
+    if request.method == "POST":
+        form = NameForm(request.POST)
+        if form.is_valid():
+            name = form.cleaned_data['your_name']
+            return render(request, 'thanks.html', {'name': name})
+    else:
+        form = NameForm()
+    return render(request, 'name.html', {'form': form})
